7ede3d3ba870566bba954d3baabb7ca0c1dd8f8e
anu-singh2507 <anusingh.barsha@gmail.com>
2026-01-16 22:03:21 +0530

    initial commit

---
+++ employee/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path,include
+from . import views
+from django.conf.urls import static
+
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('add/',views.add,name='add'),
+    path('view/',views.view,name='view'),
+    path('delete/',views.delete,name='delete'),
+    path('delete/<int:emp_id>',views.delete,name='delete'),
+    path('filter/',views.filter,name='filter'),
+]
